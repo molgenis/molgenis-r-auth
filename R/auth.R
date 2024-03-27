2fe83569a136f8449f273b027f84035f28c5598f
Clemens Tolboom <c.g.w.m.tolboom@umcg.nl>
2024-03-27 14:35:51 +0100

    loop through all encode options

--- R/auth.R
+++ R/auth.R
@@ -60,22 +60,40 @@ discover <- function(auth_server) {
 #'
 #' @export
 device_flow_auth <-
-  function(endpoint, client_id, scopes = c("openid", "offline_access"),
-    encode = c("json", "form")
-  ) {
-    encode <- match.arg(encode)
+  function(endpoint, client_id, scopes = c("openid", "offline_access")) {
+    encoding_options <- c("json", "form", "multipart", "raw")
+    encode <- NULL
+
     stopifnot(
       inherits(endpoint, "oauth_endpoint"),
       is.character(client_id),
       is.character(endpoint$device)
     )
-    response <- httr::POST(endpoint$device,
-      encode = encode,
-      body = list(
-        client_id = client_id,
-        scope = paste(scopes, collapse = " ")
-      )
-    )
+
+    response = list(status_code = 415)
+    attempt <- 0
+
+    while(response$status_code != 200 & attempt < length(encoding_options)) {
+      attempt <- attempt + 1
+      encode <- encoding_options[attempt]
+      try({
+          response <- httr::POST(
+            endpoint$device,
+            encode = encode,
+            body = list(
+              client_id = client_id,
+              scope = paste(scopes, collapse = " ")
+            )
+          )
+          print(paste0(response$status_code, encode))
+          print(paste0(response$body))
+      })
+    }
+
+    if(response$status_code != 200){
+      stop("No encoding methods worked")
+    }
+
     httr::stop_for_status(response,
       task = "initiate OpenID Device Flow authentication"
     )
